2b3726bdbdcdf596a3682cb50dc97f7135637adc
yousefturin <94796673+yousefturin@users.noreply.github.com>
2023-04-09 22:41:15 +0300

    Download image is done

--- codes/app.py
+++ codes/app.py
@@ -1,10 +1,10 @@
 import os
-from flask import Flask, flash, request, render_template, redirect, url_for,send_from_directory ,abort, jsonify
+from PIL import Image
+from flask import Flask, flash, request, make_response, render_template, redirect, url_for, send_file, send_from_directory ,abort, jsonify
 from werkzeug.utils import secure_filename
 
 
-
-UPLOAD_FOLDER = 'your/path/to/this/folder/codes/uploads'#change this path ---------------------!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+UPLOAD_FOLDER = 'codes/static/uploads'#----------------------------------------------------change this !!!1
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 app = Flask(__name__)
 
@@ -46,25 +46,55 @@ def home():
      
 
 
-@app.route('/',methods=['GET','POST'])
+@app.route('/upload',methods=['GET','POST'])
 def upload_image():
-
-        if 'file'not in request.files:
+    if request.method == 'POST':
+        file = request.files['file']  
+        if 'file' not in request.files:
+             flash('No Image Part')
              return redirect(request.url)
-        file = request.files['file']
-        if file.filename == '':
-             return redirect(request.url)
-        if file and allowed_file(file.filename):
-             filename =secure_filename(file.filename)
-             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-             return render_template('main.html', filename=filename)
         else:
+            if file.filename == '':
+                flash('No Selected Image')
+                return redirect(request.url)
+            if file and allowed_file(file.filename):
+                try:
+                    filename =secure_filename(file.filename)
+                    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                    return render_template('main.html', filename=filename,file=file)
+                except:
+                    raise ResourceNotFoundError("Image Resource could not be processed")              
+            elif file.filename not in ALLOWED_EXTENSIONS :
+                flash('Allowed image types are \n (png, jpg, jpeg, gif)')
+                return redirect(request.url)           
+            else:
+                    raise ResourceNotFoundError("Image Resource could not be retuned")  
+    else:
              return redirect(request.url)
+
         
 @app.route('/display/<filename>')
 def display_image(filename):
      return redirect(url_for('static', filename='uploads/'+ filename), code=301)
+
+
+
+
         
-        
+@app.route('/download/<filename>', methods=['GET', 'POST'])
+def download_file(filename):
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    new_filename = request.form.get('textfilename')
+    image_format = request.form.get('image_format_selector').lower()
+    image_quality = request.form.get('image_quality_selector')
+    os.path.splitext(filename)
+    filename = new_filename + image_format
+    root_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    img = Image.open(file_path)
+    img = img.convert('RGB')
+    new_quality = int(image_quality.strip('%'))
+    output_img =img.save(root_path, quality=new_quality)
+    return send_file(root_path, output_img, as_attachment=True)
+
 if __name__ == "__main__":
     app.run(debug=True)
